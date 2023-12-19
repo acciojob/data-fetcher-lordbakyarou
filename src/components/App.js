f611fbeb93e1baa21f787812a4f70d46ee3488b8
Mayur Hanwate <frostblizard111@gmail.com>
2023-12-19 05:53:56 +0530

    g

--- src/components/App.js
+++ src/components/App.js
@@ -7,17 +7,15 @@ const App = () => {
 
   useEffect(() => {
     Axios.get("https://dummyjson.com/products")
-      .then((res) => setApiFetch(res.data.products))
+      .then((res) => setApiFetch(res.data))
       .catch((err) => console.log(err));
   }, []);
 
   return (
     <div>
       <h1>Data Fetched from API</h1>
-      {console.log(apiFetch[0] === undefined, apiFetch)}
-      {(apiFetch[0] != undefined && (
-        <pre>{JSON.stringify(apiFetch, null, 2)}</pre>
-      )) ||
+      {console.log(apiFetch.products, apiFetch)}
+      {(apiFetch.products && <pre>{JSON.stringify(apiFetch, null, 2)}</pre>) ||
         "An error occurred: "}
     </div>
   );
